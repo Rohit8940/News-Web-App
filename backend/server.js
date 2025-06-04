47d43b7761cbafc35f42de6b1825b31ba8ebe76b
Rohit <baruahrohit222@gmail.com>
2025-06-04 17:02:36 +0530

    fix

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
+import cors from 'cors';
 
 const express = require("express");
 const connectDB = require("./config/db");
